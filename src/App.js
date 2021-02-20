7a8c31dba47e01a6cef7e795e23f761f07fc2210
iikim <iikim2511@gmail.com>
2021-02-19 22:29:07 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '강남 고기 맛집';
+
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집']);
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+      <div>개발 blog</div>
+    </div>
+    <div className="list">
+      <h3>{ title[1] }</h3>
+      <p>2월 17일 발행</p>
+      
+    </div>
+      
     </div>
   );
 }
