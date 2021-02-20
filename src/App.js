cb0527b36404080882a16cad246ad1a00918e80c
iikim <iikim2511@gmail.com>
2021-02-19 23:34:32 -0500

    6 props

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  
+
+  {/* 글제목 */}
   let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let posts = '강남 고기 맛집';
 
-    {/* 1 */}
+  {/* 1 */}
   const [thumb, setThumb] = useState(0);
-
-  {/* 2 */}
-  // function titleChange(){
-  //   var newArray = [...title];
-  //   newArray[0] = 'girl 코트 추천';
-  //   setTitle(newArray);
-  // }
-
 
   {/* 5 */}
   const [modalVar, setmodalVar] = useState(false);
@@ -30,39 +23,38 @@ function App() {
         <div>Shoping shoes</div>
       </div>
 
-      {/* 2 */}
-      {/* <button onClick={titleChange}>btn</button> */}
-
-       {/* 4 */}
-       {/* 5 */}
-       <button onClick={()=>{ setmodalVar(true)}}>modal show</button>
-      {
-        modalVar ===true
-        ?<Modal></Modal>
-        :null        
-      }
+     
 
       {/* 6 */}
       {
         title.map(function(a,i){
           return <div className="list">
           <h3 >{a}</h3>
+          {/* 1 */}
           <button onClick={()=>{setThumb(thumb+1)}}>👍</button> {thumb}
           <p>Feb 20</p>        
         </div>   
         })
       }
 
+      {/* 4 */}
+       {/* 5 */}
+       <button onClick={()=>{ setmodalVar(true)}}>modal show</button>
+      {
+        modalVar ===true
+        ?<Modal titleNaming={title}></Modal>
+        :null        
+      }
 
     </div>
   );
 }
 
 {/* 4 */}
-function Modal(){
+function Modal(props){
   return(
     <div className="modal">
-    <h2>제목</h2>
+    <h2>제목 : {props.titleNaming[0]}</h2>
     <p>날짜</p>
     <p>상세내용</p>
   </div>
