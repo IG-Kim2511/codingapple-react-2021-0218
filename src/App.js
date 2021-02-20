72cdf39b989d4092dabb6cb170656fdab5b4d1f8
iikim <iikim2511@gmail.com>
2021-02-19 23:02:23 -0500

    {/* 4 */}       <Modal></Modal>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
+/* eslint-disable */
+{/* 2 */}
 
 import React,{useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집';
-
-  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집']);
   
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let posts = '강남 고기 맛집';
+  const [thumb, setThumb] = useState(0);
 
+  {/* 2 */}
+  // function titleChange(){
+  //   var newArray = [...title];
+  //   newArray[0] = 'girl 코트 추천';
+  //   setTitle(newArray);
+  // }
 
   return (
     <div className="App">
       <div className="black-nav">
-      <div>개발 blog</div>
-    </div>
-    <div className="list">
-      <h3>{ title[1] }</h3>
-      <p>2월 17일 발행</p>
-      
-    </div>
-      
+        <div>Shoping shoes</div>
+      </div>
+
+      {/* 2 */}
+      {/* <button onClick={titleChange}>btn</button> */}
+
+      <div className="list">
+        <h3>{ title[0] } <button onClick={()=>{setThumb(thumb+1)}}>👍</button> {thumb} </h3>
+        <p>2월 17일 발행</p>        
+      </div>      
+      <div className="list">
+        <h3>{ title[1] }</h3>
+        <p>2월 18일 발행</p>        
+      </div>      
+      <div className="list">
+        <h3>{ title[2] }</h3>
+        <p>2월 19일 발행</p>        
+      </div>      
+
+       {/* 4 */}
+      <Modal></Modal>
+
     </div>
   );
 }
 
+{/* 4 */}
+function Modal(){
+  return(
+    <div className="modal">
+    <h2>제목</h2>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+
+  )
+}
+
 export default App;
+
